05d724b99107b4729eb0d59b1e990cd00dff25e2
Ryoya Tamura <lollipop@simochee.net>
2024-05-14 21:44:11 +0900

    feat: deploy to gh-pages (#4)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai -->
    
    ## Summary by CodeRabbit
    
    - **新機能**
      - GitHub Pagesへのデプロイ用のGitHub Actionsワークフローを追加しました。
    
    - **設定変更**
      - `astro.config.ts`にて、`site`および`base`の設定を追加しました。
    
    - **ドキュメント**
      - `src/env.d.ts`に`types.d.ts`への参照パスを追加しました。
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+	site: import.meta.env.ASTRO_SITE,
+	base: import.meta.env.ASTRO_BASE,
 	integrations: [tailwind()],
 	vite: {
 		plugins: [tsconfigPaths()],
