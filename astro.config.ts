3e00b8d002f8a2068bc1ba64ea7007d8b806e567
simochee <lollipop@simochee.net>
2024-05-14 22:03:16 +0900

    ci: remove debug code

--- astro.config.ts
+++ astro.config.ts
@@ -2,9 +2,6 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-console.log("ASTRO_SITE", process.env.ASTRO_SITE);
-console.log("ASTRO_SITE", import.meta.env.ASTRO_SITE);
-
 export default defineConfig({
 	site: process.env.ASTRO_SITE,
 	base: process.env.ASTRO_BASE,
