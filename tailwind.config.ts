006c20c935f8ea083614d56c89823ef806e355fb
Ryoya Tamura <lollipop@simochee.net>
2024-05-15 12:21:38 +0900

    feat: daisyui and layout styles (#12)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai -->
    
    ## Summary by CodeRabbit
    
    - **新機能**
      - 日本語フォント「IBM Plex Sans JP」と「IBM Plex Mono」を追加。
      - Tailwind CSSのTypographyプラグインとDaisyUIプラグインを追加。
    
    - **スタイル**
      - レイアウトのヘッダー、メインコンテンツ、フッターの構造を更新。
      - フロントエンド用語ページのレイアウトを改善し、スタイルを適用。
      - ページのコンテンツを`<article>`タグで包み、視覚的なプレゼンテーションを向上。
    
    - **依存関係**
      - 新しいフォントとプラグインの依存関係を追加。
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,21 @@
+import { family as IBMPlexMonoFamily } from "@fontsource/ibm-plex-mono/metadata.json";
+import { family as IBMPlexSansJPFamily } from "@fontsource/ibm-plex-sans-jp/metadata.json";
+import typography from "@tailwindcss/typography";
+import daisyUI, { type Config as DaisyUIConfig } from "daisyui";
 import type { Config } from "tailwindcss";
 
 export default {
 	content: ["src/**/*.{astro,ts,tsx}"],
 	theme: {
 		extend: {},
+		fontFamily: {
+			sans: [IBMPlexSansJPFamily, "sans-serif"],
+			mono: [IBMPlexMonoFamily, "monospace"],
+		},
 	},
-	plugins: [],
+	plugins: [typography, daisyUI],
+	daisyui: {
+		themes: ["nord", "dracula"],
+		darkTheme: "dracula",
+	} satisfies DaisyUIConfig,
 } satisfies Config;
