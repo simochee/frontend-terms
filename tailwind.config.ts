8d69dbdda784e128896df0a570fbca4b05f6fa9a
simochee <lollipop@simochee.net>
2024-05-15 11:54:46 +0900

    feat: setup daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,5 @@
+import typography from "@tailwindcss/typography";
+import daisyUI from "daisyui";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -5,5 +7,5 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [typography, daisyUI],
 } satisfies Config;
