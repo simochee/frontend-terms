f5c2f7f918f6407a7858c9f8bd1f665f041e4948
simochee <lollipop@simochee.net>
2024-05-15 12:03:15 +0900

    feat: fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,5 @@
+import { family as IBMPlexMonoFamily } from "@fontsource/ibm-plex-mono/metadata.json";
+import { family as IBMPlexSansJPFamily } from "@fontsource/ibm-plex-sans-jp/metadata.json";
 import typography from "@tailwindcss/typography";
 import daisyUI, { type Config as DaisyUIConfig } from "daisyui";
 import type { Config } from "tailwindcss";
@@ -6,6 +8,10 @@ export default {
 	content: ["src/**/*.{astro,ts,tsx}"],
 	theme: {
 		extend: {},
+		fontFamily: {
+			sans: [IBMPlexSansJPFamily, "sans-serif"],
+			mono: [IBMPlexMonoFamily, "monospace"],
+		},
 	},
 	plugins: [typography, daisyUI],
 	daisyui: {
